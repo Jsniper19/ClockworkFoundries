b92b825d2f3475e38dc0023891c1768b5aa8c255
Jasper Eyers <jaspereyers@hotmail.com>
2022-03-30 09:21:07 +1100

    added gameManager and playerController

---
+++ EurekaBreakthrough/Assets/scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+    public int maxInitiative;
+    public int currentInitiative;
+    public bool isPresent;
+    public float timeMeter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        currentInitiative = maxInitiative;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
