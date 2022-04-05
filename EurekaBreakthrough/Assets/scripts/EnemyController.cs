09570258d08dfbadd8c833c63fb3327bdd408f5e
Jasper eyers <jaspereyers@hotmail.com>
2022-04-05 12:17:12 +1000

    added directional logic to enemy move

--- EurekaBreakthrough/Assets/scripts/EnemyController.cs
+++ EurekaBreakthrough/Assets/scripts/EnemyController.cs
@@ -6,6 +6,9 @@ public class EnemyController : MonoBehaviour
 {
     public float maxHealth;
     public float currentHealth;
+    public GameObject PlayerCharacter;
+    public GridManager GM;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +29,24 @@ public class EnemyController : MonoBehaviour
 
     void Move()
     {
+        var RelPosX = transform.position.x - PlayerCharacter.transform.position.x;
+        var RelPosY = transform.position.y - PlayerCharacter.transform.position.y;
 
+        if (RelPosX > -RelPosY || RelPosX < RelPosY)
+        {
+            //up
+        }
+        else if (RelPosX > RelPosY || RelPosX < -RelPosY)
+        {
+            //down
+        }
+        else if (RelPosX > -RelPosY || RelPosX > RelPosY)
+        {
+            //right
+        }
+        else if (RelPosX < RelPosY || RelPosX < -RelPosY)
+        {
+            //left
+        }
     }
 }
---
+++ EurekaBreakthrough/Assets/scripts/GridManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public float TileX;
+    public float TileY;
+}
