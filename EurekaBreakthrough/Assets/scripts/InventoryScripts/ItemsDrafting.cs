f91a5a89e2cfd67994eab84c897924dcb21dce49
Hamish Redpath <49421605+hamishRedpath@users.noreply.github.com>
2022-04-06 12:23:05 +1000

    added poison damage in comments,  waiting on turnorder functionallity

--- EurekaBreakthrough/Assets/scripts/InventoryScripts/ItemsDrafting.cs
+++ EurekaBreakthrough/Assets/scripts/InventoryScripts/ItemsDrafting.cs
@@ -8,14 +8,15 @@ public class ItemsDrafting : MonoBehaviour
     public bool canAttack;
     [Header("Enter Weapon names with capital letters and spaces, IE War Club")]
     public string equippedRanged;
-    public string EquippedMelee;
+    public string equippedMelee;
     int melee, ranged;
+    public float poisonDamageMod;
 
     #region Weapons
     // data structure for weapons
     public class Weapon
     {
-        // weapon vars, can add the gameobject itself later
+        // weapon vars, can add the gameobject itself later ?? Maybe add ammou type here to make counting ammo of diff types easier
         public string name { get; set; }
         public bool ranged { get; set; }
         public float accuracy { get; set; }
@@ -34,7 +35,7 @@ public class ItemsDrafting : MonoBehaviour
             critChance = _critChance;
         }
 
-        // public override string ToString() => $"({name}, {ranged}, {accuracy}, {dmg}, {dmgType}, {critChance})";
+        public override string ToString() => $"({name}, {ranged}, {accuracy}, {dmg}, {dmgType}, {critChance})";
     }
 
     // storing weapon stats 
@@ -58,13 +59,15 @@ public class ItemsDrafting : MonoBehaviour
             {
                 ranged = i;
             }
-            else if (weapons[i].name == EquippedMelee)
+            else if (weapons[i].name == equippedMelee)
             {
                 melee = i;
             }
         }
 
         print(weapons[2]);
+
+        Attack(weapons[melee]);
     }
 
     void Update()
@@ -103,8 +106,20 @@ public class ItemsDrafting : MonoBehaviour
         System.Random rnd = new System.Random();
         if (rnd.Next(0, 100) <= weapon.accuracy)
         {
-            float dmgtotake = rnd.Next(0, 100) <= weapon.critChance ? weapon.dmg * 2 : weapon.dmg;
-           // PlayerController_Combat.maxHealth -= dmgtotake; need maxhealth to be a static var
+            float dmgToTake = rnd.Next(0, 100) <= weapon.critChance ? weapon.dmg * 2 : weapon.dmg; // need to change this to include defense stats too
+            // PlayerController_Combat.maxHealth -= dmgtotake; need maxhealth to be a static var in playercontroller
+            print(dmgToTake);
+
+            // checking to deal poison damage
+            if (weapons[melee].name == "Poison Spear")
+            {
+                /* count turns, will add this when we have turn order finished
+                if (turnCount >= 3)
+                {
+                    PlayerController_Combat.maxHealth -= weapons[melee].dmg * poisonDamageMod;
+                }
+                 */
+            }
         }
         else
         {
