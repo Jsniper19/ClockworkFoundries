3ec02614eb44b47214a3400d0cd2bb0383a5afbb
Hamish Redpath <49421605+hamishRedpath@users.noreply.github.com>
2022-04-11 09:36:37 +1000

    added enemyattack functionality with different weapons

--- EurekaBreakthrough/Assets/scripts/EnemyController.cs
+++ EurekaBreakthrough/Assets/scripts/EnemyController.cs
@@ -14,21 +14,66 @@ public class EnemyController : MonoBehaviour
 
     public int Ammo;
 
-    public float DamageLong;
-    public string TypeLong;
-    public float AccuracyLong;
-    public float CritLong;
+    public float poisonDamageMod;
+    [Header("Enter Weapon names with capital letters and NO spaces, IE WarClub")]
+    public string equippedRanged;
+    public string equippedMelee;
+    int melee, ranged;
 
-    public float DamageClose;
-    public string TypeClose;
-    public float AccuracyClose;
-    public float CritClose;
+    #region Weapons
+    // weapon class to store/access weapons easily
+    public class Weapon
+    {
+        // weapon vars, can add the gameobject itself later ?? Maybe add ammou type here to make counting ammo of diff types easier
+        public string name { get; set; }
+        public bool ranged { get; set; }
+        public float accuracy { get; set; }
+        public float dmg { get; set; }
+        public string dmgType { get; set; }
+        public float critChance { get; set; }
 
+        public Weapon(string _name, bool _ranged, float _accuracy, float _dmg, string _dmgType, float _critChance)
+        {
+            name = _name;
+            ranged = _ranged;
+            accuracy = _accuracy;
+            dmg = _dmg;
+            dmgType = _dmgType;
+            critChance = _critChance;
+        }
+
+        public override string ToString() => $"({name}, {ranged}, {accuracy}, {dmg}, {dmgType}, {critChance})";
+    }
+
+    // storing weapon stats 
+    public List<Weapon> weapons = new List<Weapon>(){
+        new Weapon("WarClub", false, 80f, 50f, "bashing", 15f),
+        new Weapon("Sword", false, 85f, 32f, "slashing", 20f),
+        new Weapon("PoisonSpear", false, 85f, 25f, "piercing", 10f),
+        new Weapon("SteamCannon", true, 80f, 60f, "piercing", 5f),
+        new Weapon("BoomStick", true, 70f, 50f, "piercing", 15f),
+        new Weapon("HuntingBoomerang", true, 90f, 40f, "bashing", 20f),
+        new Weapon("Rifle", true, 75f, 45f, "piercing", 15f)
+    };
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+
+        // find equipped weapon
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            if (weapons[i].name == equippedRanged)
+            {
+                ranged = i;
+            }
+            else if (weapons[i].name == equippedMelee)
+            {
+                melee = i;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -42,7 +87,7 @@ public class EnemyController : MonoBehaviour
             }
             else
             {
-                Attack();
+                Attack(weapons[melee]);
             }
         }
         else
@@ -51,7 +96,7 @@ public class EnemyController : MonoBehaviour
             {
                 if (MediumRange.InRange)
                 {
-                    Attack();
+                    Attack(weapons[ranged]);
                 }
                 else
                 {
@@ -62,16 +107,30 @@ public class EnemyController : MonoBehaviour
     }
 
 
-    //Hamish complete this to get the enemy to attack
-    void Attack()
+    void Attack(Weapon weapon)
     {
-        if (Ammo < 0)
+        // generate random number to determine if it hits
+        System.Random rnd = new System.Random();
+        if (rnd.Next(0, 100) <= weapon.accuracy)
         {
+            float dmgToTake = rnd.Next(0, 100) <= weapon.critChance ? weapon.dmg * 2 : weapon.dmg; // need to change this to include defense stats too
+            PlayerController_Combat.currentHealth -= dmgToTake;   
+            print(dmgToTake);
 
+            // checking to deal poison damage
+            if (weapons[melee].name == "Poison Spear")
+            {
+                /* count turns, will add this when we have turn order finished
+                if (turnCount >= 3)
+                {
+                    PlayerController_Combat.maxHealth -= weapons[melee].dmg * poisonDamageMod;
+                }
+                 */
+            }
         }
         else
         {
-
+            // misses, play miss animation
         }
     }
 
--- EurekaBreakthrough/Assets/scripts/PlayerController_Combat.cs
+++ EurekaBreakthrough/Assets/scripts/PlayerController_Combat.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController_Combat : MonoBehaviour
 {
     public float maxHealth;
-    public float currentHealth;
+    public static float currentHealth; // need health to be static to be accessed to by enemy attack script
     public int maxInitiative;
     public int currentInitiative;
     public bool isPresent;
