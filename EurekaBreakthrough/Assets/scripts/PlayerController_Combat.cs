86612cedd87b3bb2a8ca84bd48a70ca7aa1143df
Hamish Redpath <49421605+hamishRedpath@users.noreply.github.com>
2022-04-26 16:49:43 +1000

    started making player attack

--- EurekaBreakthrough/Assets/scripts/PlayerController_Combat.cs
+++ EurekaBreakthrough/Assets/scripts/PlayerController_Combat.cs
@@ -13,13 +13,64 @@ public class PlayerController_Combat : MonoBehaviour
     public bool isSelected;
     public ColliderActivation CA;
 
+    public string equippedWeapon;
+    public Weapon _weapon;
 
+    public class Weapon
+    {
+        // weapon vars, can add the gameobject itself later ?? Maybe add ammou type here to make counting ammo of diff types easier
+        public string name { get; set; }
+        public bool ranged { get; set; }
+        public float accuracy { get; set; }
+        public float dmg { get; set; }
+        public string dmgType { get; set; }
+        public float critChance { get; set; }
 
-    // Start is called before the first frame update
+        public Weapon(string _name, bool _ranged, float _accuracy, float _dmg, string _dmgType, float _critChance)
+        {
+            name = _name;
+            ranged = _ranged;
+            accuracy = _accuracy;
+            dmg = _dmg;
+            dmgType = _dmgType;
+            critChance = _critChance;
+        }
+
+        public override string ToString() => $"({name}, {ranged}, {accuracy}, {dmg}, {dmgType}, {critChance})";
+    }
+
+    public List<Weapon> weapons = new List<Weapon>(){
+        new Weapon("WarClub", false, 80f, 50f, "bashing", 15f),
+        new Weapon("Sword", false, 85f, 32f, "slashing", 20f),
+        new Weapon("SteamCannon", true, 80f, 60f, "piercing", 5f),
+        new Weapon("BoomStick", true, 70f, 50f, "piercing", 15f),
+        new Weapon("Rifle", true, 75f, 45f, "piercing", 15f)
+    };
+
     void Start()
     {
         currentHealth = maxHealth;
         currentInitiative = maxInitiative;
+    }
+
+    public void Attack()
+    {
+        if (currentInitiative > 1)
+        {
+            for (int i = 0; i < weapons.Count; i++)
+            {
+                if (weapons[i].name == equippedWeapon)
+                {
+                    _weapon = weapons[i];
+                }
+            }
+
+            System.Random rnd = new System.Random();
+            if (rnd.Next(0, 100) <= _weapon.accuracy)
+            {
+                float dmgToTake = rnd.Next(0, 100) <= _weapon.critChance ? _weapon.dmg * 2 : _weapon.dmg;
+            }
+        }
     }
 
     void TestFunction()
